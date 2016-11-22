cfd046b21fb29c4f183b899499ab3ccd3234a82f
ShMcK <shawn.j.mckay@gmail.com>
2016-11-22 13:50:20 -0800

    recursively add nested tests
    - refactor to use promises

--- src/run-tests.js
+++ src/run-tests.js
@@ -1,8 +1,13 @@
 const Mocha = require('mocha')
-const fs = require('fs')
+const glob = require('glob')
 const { join } = require('path')
 
-const testMatch = /\.test\.js$/
+const getAllTests = (testDir) => new Promise((resolve, reject) => {
+  glob(`${testDir}/**/*.test.js`, {}, (err, files) => {
+    if (err) reject(err)
+    else resolve(files)
+  })
+})
 
 function run({ projectDir, testDir }) {
 
@@ -13,27 +18,36 @@ function run({ projectDir, testDir }) {
   })
 
   // set absolute path directory for tests
-  process.env.ROOT_DIR= join(process.cwd(), projectDir)
+  process.env.ROOT_DIR = join(process.cwd(), projectDir)
 
-  fs.readdirSync(testDir)
-    // match test files
-    .filter((file) => file.match(testMatch))
-    // add test files
-    .forEach((file) => {
-      mocha.addFile(
-        join(testDir, file)
-      )
+  // recursively get all files within directories
+  getAllTests(testDir)
+    .then((files) => {
+
+      // add each test to mocha
+      files.forEach((file) => {
+        mocha.addFile(join(process.cwd(), file))
+      })
     })
+    .then(() => {
+      // run mocha tests
+      mocha.run((failures) => {
 
-  mocha.run((failures) => {
+        process.stdout.write('running tests...')
 
-    process.stdout.write('running tests...')
-
-    process.on('exit', () => {
-      process.stdout.write('complete!\n')
-      process.exit(failures)
+        process.on('exit', () => {
+          process.stdout.write('complete!\n`')
+          if (failures.length) {
+            process.stdout.write(failures)
+          }
+          process.exit(failures)
+        })
+      })
     })
-  })
+    .catch((err) => {
+      process.stdout.write('Something went wrong loading test files.')
+      process.stdout.write(err)
+    })
 }
 
-module.exports = run;
+module.exports = run
