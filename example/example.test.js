ba74889593d5f6219b31762d2c4469a8a90c5457
ShMcK <shawn.j.mckay@gmail.com>
2016-11-21 16:55:17 -0800

    add example tests

---
+++ example/example.test.js
@@ -0,0 +1,7 @@
+const expect = require('expect')
+
+describe('some test', () => {
+  it('should run', () => {
+    expect(1 + 1 === 2)
+  })
+})
---
+++ example/something.test.js
@@ -0,0 +1,9 @@
+const expect = require('expect')
+
+console.log(process.env.ROOT_DIR)
+
+describe('some test 2', () => {
+  it('should run', () => {
+    expect(1 + 1 === 2)
+  })
+})
